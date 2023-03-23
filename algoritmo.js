a49a80d2c89ea34a9f1c1727c002e9cdc9abee14
samira garcia <samira.garcia95@gmail.com>
2023-03-23 14:29:40 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,8 @@
+document.querySelector("body").innerHTML = `
+<h1>titulo de la practica 1-1</h1>
+<h2>titulo h2</h2>
+<h3>titulo h3</h3>
+<img src="https://th.bing.com/th/id/R.2c193b9c530d299bc3236ee810c98849?rik=du2nAZDNVAaaVQ&riu=http%3a%2f%2fmisimagenesde.com%2fwp-content%2fuploads%2f2017%2f05%2fgatitos-kawaii-4.jpg&ehk=KgmkhOP6Bg3N8U1T4i7xWkaHaLNqUkCmg0eunn1nHBo%3d&risl=&pid=ImgRaw&r=0"
+    alt="gatitos dibujo 1">
+
+    <img src="imagenes/gatito kwai.jfif" alt="gatito dibujo 2"> `
